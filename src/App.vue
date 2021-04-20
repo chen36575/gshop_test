805f1cb94f86c494b871e004f01ecda7b1c58d08
chen <chen36575@foxmail.com>
2021-04-20 18:28:32 +0800

    init gshop-test

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view />
+    <footer-guide v-show="$route.meta.showFooter === true"/>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide'
+
+export default {
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style>
+
+</style>
